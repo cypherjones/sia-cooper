d5fdd88a039c80d8e52396d14ee73275d7c0908a
AJ Jones <ajjonez@icloud.com>
2017-11-04 21:52:24 -0500

    change font color on product titles during hover

--- js/main.js
+++ js/main.js
@@ -2,4 +2,7 @@ jQuery(document).ready(function($){
 	$('.product-box').mouseover(function(){
 		$('.product-title').addClass('white');
 	})
+	$('.product-box').mouseout(function(){
+		$('product-title').removeClass('white');
+	})
 });
