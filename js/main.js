ae1554bf5438ac697342dc0b79219d8af94d7e27
AJ Jones <ajjonez@icloud.com>
2017-11-04 23:29:26 -0500

    single product titles

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 jQuery(document).ready(function($){
+	//find the product box
 	$('.product-box')
 	.mouseover(function(){
+		// add the class 'white' on mouseover
 		$('.product-title', this).addClass('white');
 	})
 	.mouseout(function(){
+		// remove the class 'white' on mouseout
 		$('.product-title', this).removeClass('white');
 	})
+	// find the sinlge product box
+	// $('.product-box-s')
 });
